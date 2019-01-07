889a4ead70d3416c831d37491593f82406eaca2e
tranbaquan <tranbaquan.tbq@gmail.com>
2019-01-07 13:25:04 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:nltour_collaborator/routes.dart';
+
+void main() => runApp(NLTour());
+
+class NLTour extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NLTour Collaborator',
+      theme: ThemeData(
+        textSelectionColor: Color(0xFF008fe5),
+        cursorColor: Color(0xFF008fe5),
+        primaryColor: Color(0xFF008fe5),
+        accentColor: Color(0xFF3eb43e),
+        primaryColorDark: Colors.green,
+        fontFamily: 'Semilight'
+      ),
+//      home: LoginPage(),
+      initialRoute: '/',
+      routes: routes,
+    );
+  }
+}
